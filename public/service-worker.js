74f162a9404c3efc96b33b8e115b79b798106e55
Israel Molestina <molestinaisrael@gmail.com>
2021-04-29 14:24:44 -0400

    added manifest file and added script to start service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,22 @@
-const FILES_TO_CACHE = ["/", "/index.html", "/index.js", "/styles.css"];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/index.js",
+  "/styles.css",
+  "/manifest.webmanifest",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
 self.addEventListener("install", function (evt) {
+  // pre cache image data
+  evt.waitUntil(
+    caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transaction"))
+  );
 
   // pre cache all static assets
   evt.waitUntil(
